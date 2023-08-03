1a9717190f6ddd7ba3dd6b3666d9757cbd76e761
Angelo Lencina <angelolencina@gmail.com>
2023-08-03 16:18:16 -0300

    fixed

--- src/modules/ipremi/ipremi.module.ts
+++ src/modules/ipremi/ipremi.module.ts
@@ -9,21 +9,6 @@ import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      validationSchema: Joi.object({
-        IPREMI_CAMPAIGN_ID: Joi.string().required(),
-        IPREMI_PARTNER_ACCESS_KEY: Joi.string().required(),
-        IPREMI_LOGIN: Joi.string().required(),
-        IPREMI_PASSWORD: Joi.string().required(),
-        IPREMI_PROFILE_ID: Joi.string().required(),
-        IPREMI_ENTERPRISE_ID: Joi.string().required(),
-        IPREMI_BANK_ACCOUNT_ID: Joi.string().required(),
-        IPREMI_TOKEN_API: Joi.string().required(),
-        NODE_ENV: Joi.string()
-          .valid('development', 'production', 'local', 'homologation')
-          .default('local'),
-      }),
-    }),
     DeskbeeModule,
     TypeOrmModule.forFeature([BookingEntity, RewardEntity]),
   ],

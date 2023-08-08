f216c037431bab26d52a2e997fa492bf81170394
Angelo Lencina <angelolencina@gmail.com>
2023-08-08 18:28:35 -0300

    remove unused code

--- src/modules/controlid-cloud/dto/update-controlid.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateControlidDto } from './create-controlid.dto';
-
-export class UpdateControlidDto extends PartialType(CreateControlidDto) {}
